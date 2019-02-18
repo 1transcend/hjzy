0e2e0e34b2c8d722c8c8f8842106cd931ce3c3f8
1transcend <3203067975@qq.com>
2019-02-18 21:18:43 +0800

    首页编写

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,185 @@
 <template>
-  <div>
-    <el-button>测试</el-button>
+  <div id="app">
+    <div class="conter">
+      <div class="container">
+        <el-col :span="6">
+          <a href="javascript:;" title="IT文库(wenku.it)">
+            <img src="./assets/images/logo.png" alt="IT文库(wenku.it)">
+          </a>
+        </el-col>
+      </div>     
+    </div>
+    <!-- logo结束 -->
+    
+    <div class="nav">
+      <el-menu  :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item index="2">编程开发</el-menu-item>
+        <el-menu-item index="3">系统运维</el-menu-item>
+        <el-menu-item index="4">设计·创作</el-menu-item>
+        <el-menu-item index="5">云计算·大数据</el-menu-item>
+        <el-menu-item index="6">产品·运营·综合</el-menu-item>
+        <el-menu-item index="7">文库开源</el-menu-item>
+        <el-menu-item index="8" class="el-icon-upload2">上传文档</el-menu-item>
+        <el-menu-item index="9" class="el-icon-service">会员中心</el-menu-item>
+      </el-menu>
+    </div>
+    <!-- 导航栏结束 -->
+
+    <div class="row">
+      <el-col :span="18">
+        <div class="block">
+          <el-carousel height="316px">
+            <el-carousel-item v-for="(v,k) in img" :key="k">
+              <img :src="v" alt="">
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-col>
+      <!-- 幻灯片结束 -->
+
+      <el-col class="row-right" :span="6">
+        <div class="wenku-count">
+          <el-col :span="12">
+            <div class="help-block">收录文档</div>
+            <span class="text">6244</span>
+          </el-col>
+          <el-col :span="12">
+            <div class="help-block">注册用户</div>
+            <span class="text">63</span>
+          </el-col>
+        </div>
+
+        <div class="panel">
+          <div class="panel-body">
+            <el-form :inline="true" :model="formInline" class="demo-form-inline">
+              <el-input v-model="formInline.user" placeholder="用户名"></el-input>
+              <el-input v-model="formInline.password" placeholder="请输入登录密码"></el-input>
+              <el-form-item>
+                <el-button type="primary" @click="onSubmit">登录文库</el-button>
+              </el-form-item>
+              <div class="help-block">
+                <a class="help-left" href="">忘记密码</a>
+                <a class="help-right" href="">注册会员</a>
+              </div>
+            </el-form>
+          </div>
+        </div>
+      </el-col>
+    </div>
+    <!-- 侧边栏结束 -->
+
+    <div class="container-fluid">
+      <div class="containe">
+        <el-row :gutter="20">
+          <el-col :span="6">
+            <div class="h5">编程开发</div>
+            <div class="wenku-list">
+              <a href="">前端开发</a>
+              <a href="">后端开发</a>
+              <a href="">移动开发</a>
+              <a href="">游戏开发</a>
+              <a href="">硬件开发</a>
+              <a href="">开发工具</a>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="h5">系统运维</div>
+            <div class="wenku-list">
+              <a href="">架构</a>
+              <a href="">服务器</a>
+              <a href="">操作系统</a>
+              <a href="">网络/安全</a>
+              <a href="">考试认证</a>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="h5">设计·创作</div>
+            <div class="wenku-list">
+              <a href="">UI设计</a>
+              <a href="">平面设计</a>
+              <a href="">设计软件</a>
+              <a href="">游戏动画</a>
+              <a href="">绘画创作</a>
+              <a href="">影视后期</a>
+            </div>
+          </el-col>
+          <el-col :span="6">
+            <div class="h5">云计算·大数据</div>
+            <div class="wenku-list">
+              <a href="">云计算</a>
+              <a href="">大数据</a>
+              <a href="">数据库</a>
+              <a href="">云平台</a>
+              <a href="">人工智能</a>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+    <!-- 数据分类结束 -->
+
+    <div class="wenku-item">
+      <el-col :span="12">
+        <div class="pane">
+          <div class="pane-heading">
+            <strong>热门推荐</strong>
+          </div>
+            <div class="pane-body">
+              <a href="" class="wenku-padding">
+                <img src="./assets/images/cover-news.png" alt="">
+              </a>
+            </div>
+        </div>       
+      </el-col>
+    </div>
   </div>
 </template>
 
-<script>``
+<script>
+import img from "./assets/images/banner1.png";
+import img1 from "./assets/images/banner.png";
 export default {
-
+  data() {
+    return {
+      img:{
+        img,
+        img1
+      },
+      formInline: {
+        user: '',
+        password: ''
+      },
+      activeIndex: '1',
+      activeIndex2: '1' 
+    }
+  },
+  methods:{
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
 }
 </script>
 
 <style>
-
+  @import './assets/css/app.css';
+  *{
+    padding:0px;
+    margin:0px;
+  }
+  body{
+    background-color:#f6f6f6;
+  }
+  a{
+    color:#555;
+    cursor:pointer;
+    text-decoration:none;
+  }
+  img{
+    vertical-align:middle;
+  }
 </style>
